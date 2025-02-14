38686a1c7c2cfcad290f8e39fbf12edf0b39ab58
bneveret <eve20003@byui.edu>
2025-02-14 15:07:55 -0700

    bugs

--- routes/encounter.js
+++ routes/encounter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const encounterController = require('../controllers/encounter');
-const validateObjectId = require('../middleware/validation.js');
+const { validateObjectId } = require('../middleware/validation.js');
 
 router.get('/', encounterController.getAll);
 router.get('/:id', validateObjectId, encounterController.getSingle);
