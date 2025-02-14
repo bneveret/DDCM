acfc6d312bd72d1e91c817100a497af6ce491c9e
bneveret <eve20003@byui.edu>
2025-02-14 14:24:09 -0700

    bug fixes

--- routes/campaign.js
+++ routes/campaign.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const campaignController = require('../controllers/campaign');
+const validateObjectId = require('../middleware/validation');
 
 router.get('/', campaignController.getAll);
-router.get('/:id', campaignController.getSingle);
+router.get('/:id', validateObjectId, campaignController.getSingle);
 
 router.post('/', campaignController.createCampaign);
 router.put('/:id', campaignController.updateCampaign);
-router.delete('/:id', campaignController.deleteCampaign);
+router.delete('/:id', validateObjectId, campaignController.deleteCampaign);
 
 module.exports = router;
