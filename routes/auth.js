34a10da1491a6731b29de55ee16dbb1954c4d8d6
bneveret <eve20003@byui.edu>
2025-02-21 10:41:33 -0700

    implement Oauth

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  });
+
+router.get('/logout', (req, res) => {
+  req.logout(err => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
