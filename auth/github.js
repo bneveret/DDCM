0c4fd045d8399e8221956b38191e56e31e30a240
bneveret <eve20003@byui.edu>
2025-02-21 10:26:40 -0700

    Set up github OAuth

---
+++ auth/github.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+const mongodb = require('../db/connect');
+require('dotenv').config();
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: 'https://ddcm.onrender.com/auth/github/callback'
+}, async (accessToken, refreshToken, profile, done) => {
+  try {
+    const usersCollection = mongodb.getDb().db().collection('users');
+    let user = await usersCollection.findOne({ githubId: profile.id });
+
+    if (!user) {
+      const newUser = {
+        githubId: profile.id,
+        username: profile.username,
+        avatar: profile.photos[0].value
+      };
+      const result = await usersCollection.insertOne(newUser);
+      user = { ...newUser, _id: result.insertedId };
+    }
+    return done(null, user);
+  } catch (err) {
+    return done(err, null);
+  }
+}));
+
+passport.serializeUser((user, done) => done(null, user._id));
+passport.deserializeUser(async (id, done) => {
+  const user = await mongodb.getDb().db().collection('users').findOne({ _id: id });
+  done(null, user);
+});
