6c6432b236db6426e3e0afe0141964de7afbb7d2
bneveret <eve20003@byui.edu>
2025-02-21 10:48:39 -0700

    require auth for post put and delete

--- routes/campaign.js
+++ routes/campaign.js
@@ -4,11 +4,16 @@ const router = express.Router();
 const campaignController = require('../controllers/campaign');
 const { validateObjectId } = require('../middleware/validation.js');
 
+const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.status(401).json({ message: 'Unauthorized' });
+  };
+
 router.get('/', campaignController.getAll);
 router.get('/:id', validateObjectId, campaignController.getSingle);
 
-router.post('/', campaignController.createCampaign);
-router.put('/:id', campaignController.updateCampaign);
-router.delete('/:id', validateObjectId, campaignController.deleteCampaign);
+router.post('/', ensureAuthenticated, campaignController.createCampaign);
+router.put('/:id', ensureAuthenticated, campaignController.updateCampaign);
+router.delete('/:id', ensureAuthenticated, validateObjectId, campaignController.deleteCampaign);
 
 module.exports = router;
--- routes/encounter.js
+++ routes/encounter.js
@@ -4,11 +4,16 @@ const router = express.Router();
 const encounterController = require('../controllers/encounter');
 const { validateObjectId } = require('../middleware/validation.js');
 
+const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.status(401).json({ message: 'Unauthorized' });
+  };
+
 router.get('/', encounterController.getAll);
 router.get('/:id', validateObjectId, encounterController.getSingle);
 
-router.post('/', encounterController.createEncounter);
-router.put('/:id', encounterController.updateEncounter);
-router.delete('/:id', validateObjectId, encounterController.deleteEncounter);
+router.post('/', ensureAuthenticated, encounterController.createEncounter);
+router.put('/:id', ensureAuthenticated, encounterController.updateEncounter);
+router.delete('/:id', ensureAuthenticated, validateObjectId, encounterController.deleteEncounter);
 
 module.exports = router;
