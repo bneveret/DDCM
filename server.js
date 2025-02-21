9493dd41b5ffb499eee6635de7ea8ca157c8e463
bneveret <eve20003@byui.edu>
2025-02-21 16:54:18 -0700

    fixing swagger authorization

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
