8a7cfed56caef42a73e2f4d3f22eca7de5ceabe4
bneveret <eve20003@byui.edu>
2025-02-21 16:20:12 -0700

    setting logout redirect

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,14 @@ const router = express.Router();
 
 router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
 
-router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/', successRedirect: '../../api-docs' })
+router.get('/github/callback', passport.authenticate('github', { failureRedirect: '../../api-docs', successRedirect: '../../api-docs' })
 );
 
 router.get('/logout', (req, res, next) => {
   req.logout(err => {
     if (err) return next(err);
     req.session.destroy(() => {
-      res.redirect('/');
+      res.redirect('../../api-docs');
     });
   });
 });
